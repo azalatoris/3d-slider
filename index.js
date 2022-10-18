05a9b62c33945d16897f09a743ecf96f2ee99e44
Algirdas Zalatoris <zalatoris.algirdas@toptal.com>
2022-10-18 17:39:10 +0300

    Add scroll optimization module

--- index.js
+++ index.js
@@ -6,6 +6,46 @@ const frameDistance = 100
 
 let scene, frames, fullSliderLength;
 
+const scrollModule = (scene, fullSliderLength) => {
+  const optimizationCssClass = "scene-frame-optimization"
+
+  let optimization = false
+  let scrollTimerId = undefined
+
+  const beforeAction = () => {
+    if (!optimization) {
+      scene.classList.add(optimizationCssClass)
+      optimization = true
+      console.log("Before Scroll")
+    }
+  }
+
+  const afterAction = () => {
+    clearTimeout(scrollTimerId)
+
+    scrollTimerId = setTimeout(() => {
+      optimization = false
+      scene.classList.remove(optimizationCssClass)
+      console.log("After Scroll")
+    }, 1000)
+  }
+
+  const scrollHandler = () => {
+    beforeAction()
+
+    console.log("Scroll")
+    const scrollPosition = document.documentElement.scrollTop
+    const correctedScrollPosition = proportionalPageScrollPosition(scrollPosition, fullSliderLength)
+    
+    setSceneScrollPosition(scene, correctedScrollPosition)
+
+    afterAction()
+  }
+
+  return {
+    scrollHandler: scrollHandler
+  }
+}
 // Page onload
 window.on('load', () => {
   console.log('Page is loaded')
@@ -16,15 +56,12 @@ window.on('load', () => {
 
   setupInitialFramesPosition(frames, frameDistance)
   framesPrescroll()
+
+  // Page onscroll
+  window.on('scroll', 
+    throttle(
+      100, 
+      scrollModule(scene, fullSliderLength).scrollHandler
+    )
+  ) 
 })
-
-const scrollHandler = () => {
-  console.log("Scroll")
-  const scrollPosition = document.documentElement.scrollTop
-  const correctedScrollPosition = proportionalPageScrollPosition(scrollPosition, fullSliderLength)
-  
-  setSceneScrollPosition(scene, correctedScrollPosition)
-}
-
-// Page onscroll
-window.on('scroll', throttle(100, scrollHandler) ) 
